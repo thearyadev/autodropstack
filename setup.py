b9220dce62bb807fd94360d7041760e86349e8db
thearyadev <personal@aryankothari.dev>
2023-08-30 20:58:58 -0400

    fixes for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="autodropstack",
-    version="0.1.0",
+    version="0.1.1",
     description="filo stack",
     author="thearyadev",
-    packages=["autodropstack"],
+    packages=find_packages(),
+    package_data={"": ["*.txt", "*.md"]},
+    long_description=open("README.md").read(),
 )
