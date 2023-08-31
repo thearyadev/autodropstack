60bda14f85e1bd1f2ec6e479fbcb3acea4f80906
thearyadev <personal@aryankothari.dev>
2023-08-30 20:36:50 -0400

    change vernum

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="autodropstack",
-    version="0.0.1",
+    version="0.1.0",
     description="filo stack",
     author="thearyadev",
     packages=["autodropstack"],
