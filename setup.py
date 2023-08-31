a4077cb4b01644c9ef5f5bb433ac0e69d4ebcf00
thearyadev <personal@aryankothari.dev>
2023-08-30 21:15:22 -0400

    fix markdown usage error (twine error)

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     author="thearyadev",
     packages=find_packages(),
     package_data={"": ["*.txt", "*.md"]},
+    long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
 )
